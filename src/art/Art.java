7f7213931a349e6f29674352662a45b32f7ed573
Aluno <Aluno@LAB04-PC05>
2024-06-04 20:59:38 -0300

    Bandeira do Brazil

---
+++ src/art/Art.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package art;
+
+/**
+ *
+ * @Luiz
+ */
+public class Art {
+
+    public static void main(String[] args) {
+        
+        System.out.println("XXXXXXXXXXXXXXX");
+        System.out.println("X             X");
+        System.out.println("X      X      X");
+        System.out.println("X    X   X    X");
+        System.out.println("X  X       X  X");
+        System.out.println("X X   ###   X X");
+        System.out.println("X X   ###   X X");
+        System.out.println("X  X       X  X");
+        System.out.println("X    X   X    X");
+        System.out.println("X      X      X");
+        System.out.println("X             X");
+        System.out.println("XXXXXXXXXXXXXXX");
+    }
+}
